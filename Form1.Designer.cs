a99a97117bd3a7eb29a41c58a9fc1d9889299ed8
Michww3 <michww3@gmail.com>
2025-06-04 14:44:59 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButtn = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.StopButtton = new System.Windows.Forms.Button();
+            this.TimerTextBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButtn
+            // 
+            this.StartButtn.Location = new System.Drawing.Point(137, 301);
+            this.StartButtn.Name = "StartButtn";
+            this.StartButtn.Size = new System.Drawing.Size(144, 72);
+            this.StartButtn.TabIndex = 0;
+            this.StartButtn.Text = "Start";
+            this.StartButtn.UseVisualStyleBackColor = true;
+            this.StartButtn.Click += new System.EventHandler(this.StartButtn_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(459, 301);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(144, 72);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // StopButtton
+            // 
+            this.StopButtton.Location = new System.Drawing.Point(300, 301);
+            this.StopButtton.Name = "StopButtton";
+            this.StopButtton.Size = new System.Drawing.Size(144, 72);
+            this.StopButtton.TabIndex = 2;
+            this.StopButtton.Text = "Stop";
+            this.StopButtton.UseVisualStyleBackColor = true;
+            this.StopButtton.Click += new System.EventHandler(this.StopButtton_Click);
+            // 
+            // TimerTextBox
+            // 
+            this.TimerTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimerTextBox.Location = new System.Drawing.Point(228, 117);
+            this.TimerTextBox.Name = "TimerTextBox";
+            this.TimerTextBox.Size = new System.Drawing.Size(301, 90);
+            this.TimerTextBox.TabIndex = 6;
+            this.TimerTextBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TimerTextBox);
+            this.Controls.Add(this.StopButtton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StartButtn);
+            this.Name = "Form1";
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButtn;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button StopButtton;
+        private System.Windows.Forms.Label TimerTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer timer;
+        int totalMilliseconds = 0;
+        const string timerDefault = "00:00:00:000";
+
+        public Form1()
+        {
+            InitializeComponent();
+            TimerTextBox.Text = timerDefault;
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += UpdateTextBox;
+        }
+
+        private void StartButtn_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void StopButtton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            totalMilliseconds = 0;
+            TimerTextBox.Text = timerDefault;
+        }
+        public void UpdateTextBox(object sender, EventArgs e)
+        {
+            totalMilliseconds += 1000;
+            int hours = totalMilliseconds / 3600000;
+            int minutes = (totalMilliseconds % 3600000) / 60000;
+            int seconds = (totalMilliseconds % 60000) / 1000;
+            int milliseconds = totalMilliseconds % 1000;
+
+            TimerTextBox.Text = $"{hours:D2}:{minutes:D2}:{seconds:D2}:{milliseconds:D3}";
+        }
+    }
+}
